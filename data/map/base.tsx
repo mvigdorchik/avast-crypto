1a1a229322625d78951a822222584b79d9cb7352
Lawrence Berry <lawrenceberry@outlook.com>
2020-01-18 18:21:36 +0000

    Level 1 map

--- data/map/base.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="base" tilewidth="64" tileheight="64" tilecount="196" columns="14">
- <image source="../img/Platformer Art Complete Pack/Base pack/Tiles/tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
--- js/screens/play.js
+++ js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
      */
     onResetEvent: function () {
         // load a level
-        me.levelDirector.loadLevel("area01")
+        me.levelDirector.loadLevel("level_1")
 
         // reset the score
         game.data.score = 0;
