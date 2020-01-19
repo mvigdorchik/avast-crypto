73c58829149ae5ed0a4e449dd5462d1e1a5a0954
Lawrence Berry <lawrenceberry@outlook.com>
2020-01-19 02:29:05 +0000

    Fixed duplicate cipher

--- js/screens/play.js
+++ js/screens/play.js
@@ -6,12 +6,6 @@ game.PlayScreen = me.ScreenObject.extend({
         // load a level
         me.levelDirector.loadLevel("level_1")
 
-        // Add our cipher to the game world, add it last so that this is on top of the rest.
-        // Can also be forced by specifying a "Infinity" z value to the addChild function.
-        // Position x and y need to be negative integers relative to bottom right
-        this.cipher = new game.cipher.Container(710, 535);
-        me.game.world.addChild(this.cipher);
-
         this.lever_list = [];
 
         this.spawnEntities("vigenere");
