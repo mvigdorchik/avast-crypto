c2674381325f1c50f254a10dc9049b79c945213a
Lawrence Berry <lawrenceberry@outlook.com>
2020-01-19 08:05:40 +0000

    Two seperate fonts for cipher and text

--- js/entities/cipher.js
+++ js/entities/cipher.js
@@ -40,7 +40,7 @@ game.cipher.text = me.Renderable.extend({
         this._super(me.Renderable, 'init', [x, y, 10, 10]);
 
         // create the font object
-        this.font = new me.BitmapFont(me.loader.getBinary('PressStart2P'), me.loader.getImage('PressStart2P'));
+        this.font = new me.BitmapFont(me.loader.getBinary('PressStart2PCipher'), me.loader.getImage('PressStart2PCipher'));
 
         // font alignment to right, bottom
         this.font.textAlign = "right";
